0949983331a074e714a9dce33c7308da6e280b6c
bonskaia <odinvoskresenskij@gmail.com>
2024-11-27 21:44:12 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	double a, b, c, d;
+
+	cout << "¬ведите числа: ";
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+	return 0;
+}
